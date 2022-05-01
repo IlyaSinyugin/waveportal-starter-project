7a5a52c728cf8c3f4351fdb70f3257ec4b7030c9
Ilya <ilya@sinyugin.ru>
2022-05-01 15:33:20 +0200

    added connect wallet btn

--- src/App.js
+++ src/App.js
@@ -1,41 +1,78 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
-    /*
-    * Check if we have acce to window.ethereum
-    */
-   const {ethereum} = window;
+  const [currentAccount, setCurrentAccount] = useState("");
 
-   if (!ethereum) {
-     console.log("Make sure you have Metamask!");
-   } else {
-     console.log("We have the Ethereum object", ethereum);
-   }
-  }
-  
-  React.useEffect(() => {
+  // Store user's public wallet
+  const checkIfWalletIsConnected = async () => {
+    try {
+      /*
+       * Check if we have acce to window.ethereum
+       */
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have Metamask!");
+      } else {
+        console.log("We have the Ethereum object", ethereum);
+      }
+
+      // Check if we can access user's wallet
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // connectWallet method
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
     checkIfWalletIsConnected();
-  }, [])
-  
+  }, []);
+
   return (
     <div className="mainContainer">
       <div className="dataContainer">
-        <div className="header">
-        👋 Hellooo!
-        </div>
+        <div className="header">👋 Hellooo!</div>
 
-        <div className="bio">
-        I am Ilya. Wave at me!
-        </div>
-        <button className="waveButton" onClick={wave}>
+        <div className="bio">I am Ilya. Wave at me!</div>
+        <button className="waveButton" onClick={null}>
           Wave
         </button>
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
-}
-
-export default App
+};
+export default App;
