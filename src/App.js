720a92dfcfa90f79adcf297ff73690c047cea2f2
Ilya <ilya@sinyugin.ru>
2022-05-01 16:37:53 +0200

    added abi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+
+  const contractAddress = "0x41855828c468F285b259180994F6aD7556ae980C";
+
+  const contractABI = abi.abi;
 
   // Store user's public wallet
   const checkIfWalletIsConnected = async () => {
@@ -53,6 +58,41 @@ const App = () => {
     }
   };
 
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+         * Execute the actual wave from your smart contract
+         */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -63,7 +103,7 @@ const App = () => {
         <div className="header">👋 Hellooo!</div>
 
         <div className="bio">I am Ilya. Wave at me!</div>
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave
         </button>
         {!currentAccount && (
